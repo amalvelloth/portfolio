338d5acfc29f03cc2560926c0a348d37b76968c3
amal velloth <amalv414@gmail.com>
2024-10-12 15:19:38 +0530

    hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,14 +3,25 @@ import React from 'react'
 function Hero() {
     return (
         <section className='hero-section'>
-            <h1 className='text-center text-4xl'>PORTFOLIO</h1>
+            <div className="frame-left hidden lg:block"></div>
+            <div className="frame-right hidden lg:block"></div>
+
+            <div className="frame-top">
+                <span className='top-line-1 hidden lg:block'></span>
+                <span className='top-line-2 hidden lg:block'></span>
+            </div>
+            <div className='container'>
+            <h1 className='text-center left-0 right-0 font-deutschlander text-4xl'>AMAL V</h1>
+            </div>
             <div className="fancy-menu">
                 <span className="menu-icon">≡</span>
                 <span className="menu-text">MENU</span>
+                <span className="line-1"></span>
+                <span className="line-2"></span>
             </div>
+
+            <div class="fade-effect absolute bottom-0 left-0 right-0"></div>
         </section>
-
-
     )
 }
 
