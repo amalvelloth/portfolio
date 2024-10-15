d3274e7be409261f8133c3ebc2561697a146f581
amal velloth <amalv414@gmail.com>
2024-10-15 18:08:19 +0530

    changed line wrapper

--- src/components/LineWrapper.jsx
+++ src/components/LineWrapper.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 function LineWrapper() {
     return (
-        <div className="line-wrapper flex ">
-            <div className="h-[1px] w-[50%] bg-[#474747]"></div>
-            <div className="h-[1px] w-[2%] bg-white rotate-45"></div>
-            <div className="h-[1px] w-[50%] bg-[#474747]"></div>
+        <div className="line-wrapper relative flex bg-black h-7">
+            <div className="h-[2px] w-[49.5%] bg-[#474747]"></div>
+            <div className="h-full w-[2px] absolute left-0 top-0 bottom-0 right-0 m-auto bg-[#474747] rotate-[-45deg]"></div>
+            <div className="h-[2px] w-[49.5%] bottom-0 right-0 absolute bg-[#474747]"></div>
         </div>
-
     )
 }
 
