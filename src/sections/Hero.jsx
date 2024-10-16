3d48b6c138ef861dfd2982756a24cc9bb28411b1
amal velloth <amalv414@gmail.com>
2024-10-16 21:06:39 +0530

    video

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import BurgerMenu from '../components/BurgerMenu'
+import blackSpace from '../assets/black_space.mp4'
 
 
 function Hero() {
@@ -8,7 +9,14 @@ function Hero() {
             <div className="fixed lg:block hidden left-4 top-[1.4rem] h-screen w-[1px] bg-[#474747]"></div>
             <div className="fixed lg:block hidden right-4 top-[1.4rem] bottom-[2rem] h-[100%] w-[1px] bg-[#474747]"></div>
             <h1 className='text-center font-deutschlander text-9xl'>AMAL V</h1>
-            <h3>Full stack Developer - Web Designer - React Developer</h3>
+            <h3 className='text-center'>Full stack Developer - Web Designer - React Developer</h3>
+
+            <video className="w-1/2 h-1/2 rounded-xl border" loop autoplay muted>
+                <source src={blackSpace} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+
+
             <div className="fancy-menu block lg:hidden">
                 <BurgerMenu />
                 <span className="menu-text">MENU</span>
