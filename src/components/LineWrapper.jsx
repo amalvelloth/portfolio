04e42abeddc0327f59b80d3379f7d9a796be78d4
amal velloth <amalv414@gmail.com>
2024-10-16 19:06:26 +0530

    update line wrapper

--- src/components/LineWrapper.jsx
+++ src/components/LineWrapper.jsx
@@ -7,12 +7,12 @@ function LineWrapper() {
         //     <div className="block relative w-[52px] h-1 bg-green-500 rotate-[45deg] flex-shrink-0"></div>
         //     <div className='block w-full h-1 bg-blue-700'></div>
         // </div>
-        
+
         <div className="flex relative justify-start items-center h-6 bg-black overflow-hidden">
-    <div className='border-t border-[#474747] w-20 h-6 mr-[-19px]'></div>
-    <div className="block w-[70px] h-[1px] bg-[#474747] rotate-[35deg] flex-shrink-0"></div>
-    <div className="border-b border-l-0 border-r-0 border-[#474747] w-full h-6 m-[-18px]"></div>
-</div>
+            <div className='border-t border-[#474747] md:w-48 w-20 h-6 mr-[-19px]'></div>
+            <div className="w-[70px] h-[1px] bg-[#474747] rotate-[35deg] flex-shrink-0"></div>
+            <div className="border-b border-l-0 border-r-0 border-[#474747] w-full h-6 m-[-18px]"></div>
+        </div>
 
     )
 }
