5bb78c941e9b93d51e8e4d55ca9ec8cfeaefc07d
amal velloth <amalv414@gmail.com>
2024-11-01 01:38:20 +0530

    pinned scroll gsap

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -1,24 +1,90 @@
-import React from 'react'
-import LineWrapper from '../components/LineWrapper'
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import Box from "@mui/material/Box";
+import LineWrapper from "../components/LineWrapper";
+import nikeAdidas from "../assets/images/nikeAdidas.png"
+import midnightMagic from "../assets/images/midnightMagic.png"
+import zenitsu from "../assets/images/zenitsu.png"
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Project() {
-  return (
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      // Set initial state for photos (excluding the first one)
+      gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 });
 
-    <>
-    <LineWrapper/>
-      <section id='project' className="h-full py-14 bg-black md:flex-col text-white flex items-center">
-        <div className="container mx-auto flex md:justify-center">
-          <div className='m-2'>
-            <h2 className="text-3xl font-bold">This is Project container</h2>
-            <p>Here will be the my Projects details.</p>
+      const animation = gsap.to(".photo:not(:first-child)", {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        stagger: 0.5, // Adjusting stagger for smoother animation
+      });
+
+      ScrollTrigger.create({
+        trigger: ".gallery",
+        start: "top top",
+        end: "bottom bottom",
+        pin: ".leftblock",
+        animation: animation,
+        scrub: true,
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <LineWrapper />
+      <Box className="gallery" sx={{ display: "flex", minHeight: "100vh", bgcolor: "black" }}>
+        {/* Left image block */}
+        <Box
+          className="leftblock"
+          sx={{
+            width: "30%",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "white",
+            bgcolor: "black",
+          }}
+        >
+          <div className="title p-3">
+            <h1 className="text-3xl" style={{ fontWeight: "bold" }}>Projects</h1>
+            <h1 className="text-2xl font-thin">These are my Projects</h1>
           </div>
-          <div className='m-2'>
-            <img className='h-auto max-w-full w-[40rem]' src="https://preview.redd.it/it9813ha66471.png?auto=webp&s=14653568fab6e7b6f6cf951fa253ed03a2575313" alt="" />
-          </div>
-        </div>
-      </section>
-    </>
-  )
+        </Box>
+
+
+        {/* Right text block */}
+        <Box
+          className="rightblock"
+          sx={{
+            width: "70%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            ml: "auto",
+            color: "#000",
+          }}
+        >
+          <Box className="details" sx={{ fontSize: "3rem", fontWeight: 900, mb: 3, color: "white" }}>
+            <img className="w-3/4" src={nikeAdidas} alt="" />
+          </Box>
+          <Box className="details" sx={{ fontSize: "3rem", fontWeight: 900, mb: 3, color: "white" }}>
+            <img className="w-3/4" src={midnightMagic} alt="" />
+          </Box>
+          <Box className="details" sx={{ fontSize: "3rem", fontWeight: 900, color: "white" }}>
+            <img className="w-3/4" src={zenitsu} alt="" />
+          </Box>
+        </Box>
+      </Box>
+    </React.Fragment>
+  );
 }
 
-export default Project
+export default Project;
