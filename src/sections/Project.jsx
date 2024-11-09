24dcbafac0e6f53e4a75a5970d9484ab18217c4c
amal velloth <amalv414@gmail.com>
2024-11-09 19:36:20 +0530

    aligned project section for small devices

--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -11,7 +11,6 @@ gsap.registerPlugin(ScrollTrigger);
 function Project() {
   useEffect(() => {
     const ctx = gsap.context(() => {
-      // Set initial state for photos (excluding the first one)
       gsap.set(".photo:not(:first-child)", { opacity: 0, scale: 0.5 });
 
       const animation = gsap.to(".photo:not(:first-child)", {
@@ -21,13 +20,18 @@ function Project() {
         stagger: 0.5,
       });
 
-      ScrollTrigger.create({
-        trigger: ".gallery",
-        start: "top top",
-        end: "bottom bottom",
-        pin: ".leftblock",
-        animation: animation,
-        scrub: true,
+      ScrollTrigger.matchMedia({
+        // Apply the pin effect for screens `md` and above
+        "(min-width: 768px)": function () {
+          ScrollTrigger.create({
+            trigger: ".gallery",
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".leftblock",
+            animation: animation,
+            scrub: true,
+          });
+        },
       });
     });
 
@@ -70,37 +74,44 @@ function Project() {
   return (
     <React.Fragment>
       <LineWrapper />
-      <div className="gallery flex min-h-screen bg-black">
+      <div className="gallery flex max-md:flex-col min-h-screen bg-black">
         {/* Left image block */}
-        <div
-          className="leftblock w-[30%] h-[100vh] flex flex-col items-center justify-center text-white bg-black"
-        >
+        <div className="leftblock w-[30%] h-[100vh] max-md:h-full max-md:w-full max-md:items-start flex flex-col items-center justify-center text-white bg-black">
           <div className="title p-2">
-            <h1 className="text-3xl" style={{ fontWeight: "bold" }}>Projects</h1>
+            <h1 className="text-3xl font-bold">Projects</h1>
             <h1 className="text-2xl font-thin">These are my Projects</h1>
           </div>
         </div>
 
         {/* Right text block */}
-        <div className="rightblock w-2/3 flex flex-col justify-center ml-auto">
+        <div className="rightblock w-2/3 max-md:w-full flex flex-col justify-center ml-auto">
           <div className="hand flex justify-center border border-gray-400 p-2">
             <h2 className="text-sm flex items-center text-black">
               Live site &#8599;
             </h2>
           </div>
           <a href="https://nike-adidas.netlify.app/" target="_blank" rel="noopener noreferrer">
-            <div className="details text-white text-3xl font-bold mb-3">
-              <img className="w-3/4 mx-auto" draggable="false" src={nikeAdidas} alt="Nike Adidas Project" />
+            <div className="details h-screen max-md:h-full max-md:py-10 text-white text-3xl font-bold mb-3">
+              <div className="box w-3/4 max-md:w-full max-md:px-2">
+                <img className="w-full" draggable="false" src={nikeAdidas} alt="Nike Adidas Project" />
+                <p className="text-sm font-thin">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tenetur praesentium eveniet distinctio dolores tempora odio animi est dolor voluptate ex</p>
+              </div>
             </div>
           </a>
           <a href="https://midnightmagic.netlify.app/" target="_blank" rel="noopener noreferrer">
-            <div className="details text-white text-3xl font-bold mb-3">
-              <img className="w-3/4 mx-auto" draggable="false" src={midnightMagic} alt="Midnight Magic Project" />
+            <div className="details h-screen max-md:h-full max-md:py-10 text-white text-3xl font-bold mb-3">
+              <div className="box w-3/4 max-md:w-full max-md:px-2">
+                <img className="w-full" draggable="false" src={midnightMagic} alt="Midnight Magic Project" />
+                <p className="text-sm font-thin">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tenetur praesentium eveniet distinctio dolores tempora odio animi est dolor voluptate ex</p>
+              </div>
             </div>
           </a>
           <a href="https://zenitsu-parallax.netlify.app/" target="_blank" rel="noopener noreferrer">
-            <div className="details text-white text-3xl font-bold">
-              <img className="w-3/4 mx-auto" draggable="false" src={zenitsu} alt="Zenitsu Parallax Project" />
+            <div className="details h-screen max-md:h-full max-md:py-10 text-white text-3xl font-bold">
+              <div className="box w-3/4 max-md:w-full max-md:px-2">
+                <img className="w-full" draggable="false" src={zenitsu} alt="Zenitsu Parallax Project" />
+                <p className="text-sm font-thin">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia tenetur praesentium eveniet distinctio dolores tempora odio animi est dolor voluptate ex</p>
+              </div>
             </div>
           </a>
         </div>
