84b48c11554b24251c72bcbfd5a7e35ccab22e4c
amal velloth <amalv414@gmail.com>
2024-11-19 00:50:47 +0530

    scollTrigger for video speed

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import space from '../assets/spacetravel.mp4'; // Import the space video
-
+import React, { useRef, useEffect } from 'react';
+import space from '../assets/spacetravel.mp4';
+import  { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 function VideoBackground() {
+    const videoRef = useRef(null);
+    
+    useEffect (() => {
+        const video = videoRef.current;
+
+        if (video) {
+            ScrollTrigger.create({
+                trigger: video,
+                start: 'top top',
+                end: 'bottom top',
+                scrub: true,
+                onUpdate: (self) => {
+                    video.playbackRate = self.direction === 1 ? 3 : 1;
+                },
+            });
+        }
+
+        return () => {
+            ScrollTrigger.killAll();
+        };
+    }, []);
+
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden -z-10 ">
+        <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden -z-10">
             <video
+                ref={videoRef}
                 className="absolute top-0 left-0 w-full h-full object-cover"
                 src={space}
                 type="video/mp4"
@@ -18,8 +41,7 @@ function VideoBackground() {
             </video>
             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60"></div>
         </div>
-        
-    )
+    );
 }
 
-export default VideoBackground
+export default VideoBackground;
