dbe511a7a2652733924d1cd04270c588b2fc1fc5
amal velloth <amalv414@gmail.com>
2025-01-21 16:55:41 +0530

    navigation in contact page

--- src/components/ContactButton.jsx
+++ src/components/ContactButton.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation } from 'react-router-dom';
 
 function ContactButton() {
   const navigate = useNavigate();
   const [isVisible, setIsVisible] = useState(true);
+  const location = useLocation();
 
   const handleNavigation = () => {
-    navigate('/contactpage');
+    // navigate('/contactpage');
+    if (location.pathname === '/contactpage') {
+      navigate('/');
+    } else {
+      navigate("/contactpage");
+    }
   };
 
   const handleScroll = () => {
@@ -25,7 +31,6 @@ function ContactButton() {
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -37,7 +42,7 @@ function ContactButton() {
       className="z-50 pulse-shadow hover:cursor m-4 fixed rounded-full right-4 bottom-4 w-24 h-24 md:w-32 md:h-32 flex items-center justify-center text-white bg-black border-[0.5px] border-[#9D9C9A]"
       onClick={handleNavigation} 
     >
-      <h2 className="text-sm">CONTACT</h2>
+      <h2 className="text-sm">{location.pathname === '/contactpage' ? 'HOME' : 'CONTACT'}</h2>
     </button>
   );
 }
